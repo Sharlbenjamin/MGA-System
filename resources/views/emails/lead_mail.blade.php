bcf8336f38d22bc21fc6f333c5b53fc04ba69f77
Sharlbenjamin <sharlhany@gmail.com>
2025-02-23 19:30:17 +0100

    oh yeah 12

--- resources/views/emails/lead_mail.blade.php
+++ resources/views/emails/lead_mail.blade.php
@@ -7,7 +7,7 @@
     <p>{!! nl2br(e($body)) !!}</p>
 
     @if(isset($signature))
-    <img src="{{ asset('storage/signatures/Charles.png') }}" 
+    <img src="{{ asset('storage/signatures/' . basename($signature)) }}" 
          alt="Signature" width="300" style="background: white; padding: 10px; border-radius: 5px;">
     @else
         <p>No signature available</p>
