3c88a7d83bf40ed629d1e5e6b944f79aa49e8797
Sharlbenjamin <sharlhany@gmail.com>
2025-04-01 23:48:37 +0200

    invoices policy

---
+++ app/Policies/InvoiceItemPolicy.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\InvoiceItem;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class InvoiceItemPolicy
+{
+    use HandlesAuthorization;
+
+    public function viewAny(User $user)
+    {
+        return $user->hasPermissionTo('view-any-invoice-items');
+    }
+
+    public function view(User $user, InvoiceItem $invoiceItem)
+    {
+        return $user->hasPermissionTo('view-invoice-items');
+    }
+
+    public function create(User $user)
+    {
+        return $user->hasPermissionTo('create-invoice-items');
+    }
+
+    public function update(User $user, InvoiceItem $invoiceItem)
+    {
+        return $user->hasPermissionTo('edit-invoice-items');
+    }
+
+    public function delete(User $user, InvoiceItem $invoiceItem)
+    {
+        return $user->hasPermissionTo('delete-invoice-items');
+    }
+}
--- app/Policies/InvoicePolicy.php
+++ app/Policies/InvoicePolicy.php
@@ -10,27 +10,27 @@ class InvoicePolicy
 {
     public function viewAny(User $user): bool
     {
-        return $user->hasPermissionTo('view Invoices');
+        return $user->hasPermissionTo('view-any-invoices');
     }
 
     public function view(User $user, Invoice $invoice): bool
     {
-        return $user->hasPermissionTo('view Invoices');
+        return $user->hasPermissionTo('view-invoices');
     }
 
     public function create(User $user): bool
     {
-        return $user->hasPermissionTo('create Invoices');
+        return $user->hasPermissionTo('create-invoices');
     }
 
     public function update(User $user, Invoice $invoice): bool
     {
-        return $user->hasPermissionTo('edit Invoices');
+        return $user->hasPermissionTo('edit-invoices');
     }
 
     public function delete(User $user, Invoice $invoice): bool
     {
-        return $user->hasPermissionTo('delete Invoices');
+        return $user->hasPermissionTo('delete-invoices');
     }
 
     public function restore(User $user, Invoice $invoice): bool
