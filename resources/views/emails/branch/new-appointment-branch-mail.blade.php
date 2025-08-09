bf5ebe686d2ba1b051251983515540da0f830227
Sharlbenjamin <sharlhany@gmail.com>
2025-08-09 14:47:42 +0300

    Request appointments

--- resources/views/emails/branch/new-appointment-branch-mail.blade.php
+++ resources/views/emails/branch/new-appointment-branch-mail.blade.php
@@ -4,23 +4,23 @@
     @if($appointment->providerBranch && $appointment->providerBranch->provider && $appointment->providerBranch->provider->status == 'Active')
     <p>Dear {{ $appointment->providerBranch->branch_name ?? 'Team' }},</p>
     <p>We are Requesting an appointment availability with the following details.</p>
-    <p><strong>MGA Reference:</strong> {{ $appointment->file->mga_reference ?? 'mga_reference' }}</p>
-    <p><strong>Patient Name:</strong> {{ $appointment->file->patient->name ?? 'patient->name' }}</p>
+    <p><strong>MGA Reference:</strong> {{ $appointment->file->mga_reference }}</p>
+    <p><strong>Patient Name:</strong> {{ $appointment->file->patient->name }}</p>
     <p><strong>Appointment Details:</strong></p>
     <ul>
     <li><strong>Date:</strong> {{ date('d-m-Y', strtotime($appointment->service_date)) }}</li>
     <li><strong>Time:</strong> {{ $appointment->service_time ?? 'N/A' }}</li>
-    <li><strong>Service:</strong> {{ $appointment->file->serviceType->name ?? 'serviceType->name' }}</li>
+    <li><strong>Service:</strong> {{ $appointment->file->serviceType->name ?? '' }}</li>
     </ul>
 
     @if($appointment->file->symptoms)
-    <p><strong>Symptoms:</strong> {{ $appointment->file->symptoms }}</p>
+    <p><strong>Symptoms:</strong> {{ $appointment->file->symptoms ?? 'N/A'}}</p>
     @endif
 
     @else
 
     <p>Dear team,</p>
-    <p>We have a patient in {{ $appointment->file->city?->name ?? 'Unknown City' }} that needs a {{ $appointment->file->serviceType->name ?? 'Unknown Service' }} appointment.</p>
+    <p>We have a patient in {{ $appointment->file->city?->name ?? 'Your City' }} that needs a {{ $appointment->file->serviceType->name ?? 'Medical' }} appointment.</p>
     <p>Symptoms: {{ $appointment->file->symptoms ?? 'N/A' }}</p>
     <p>Can you provide our patient with the required medical attention?</p>
     <p>We are Med Guard Assistance, a Medical and Travel Assistance Company, That provides cashless medical services to our patients by paying to our providers on their behalf.</p>
