4b95415e4d16b325c689fd9849cc7e91885de61c
Sharlbenjamin <sharlhany@gmail.com>
2025-08-29 16:44:06 +0300

    Some feature or fix

--- app/Filament/Pages/FileRequestAppointment.php
+++ app/Filament/Pages/FileRequestAppointment.php
@@ -10,7 +10,7 @@ use App\Models\City;
 use App\Services\GoogleDistanceService;
 use App\Mail\NotifyBranchMailable;
 use App\Models\Appointment;
-use Filament\Resources\Pages\Page;
+use Filament\Pages\Page;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
