b77928bdc5e19613ede3a332f6689c84555a2edb
Sharlbenjamin <sharlhany@gmail.com>
2025-09-03 12:32:29 +0300

    Branch availabilities

--- app/Filament/Resources/BranchAvailabilityResource/Pages/BranchAvailabilityIndex.php
+++ app/Filament/Resources/BranchAvailabilityResource/Pages/BranchAvailabilityIndex.php
@@ -351,21 +351,17 @@ class BranchAvailabilityIndex extends Page implements HasForms, HasTable
                                     <path d='M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z'></path>
                                     <path d='M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z'></path>
                                 </svg>
-                                <a href='mailto:{$record->email}' class='text-blue-600 hover:text-blue-800 underline'>{$record->email}</a>
+                                <a href='mailto:{$record->email}' class='text-blue-600 hover:text-blue-800 underline'>Email</a>
                             </div>";
                         }
                         
-                        // Get direct branch phone with click action
+                        // Get direct branch phone
                         if ($record->phone) {
                             $contactMethods[] = "<div class='flex items-center gap-1'>
                                 <svg class='w-4 h-4 text-green-500' fill='currentColor' viewBox='0 0 20 20'>
                                     <path d='M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z'></path>
                                 </svg>
-                                <button 
-                                    onclick=\"window.livewire.find('{$this->getId()}').call('showPhoneNotification', {$record->id})\"
-                                    class='text-green-600 hover:text-green-800 underline cursor-pointer'
-                                    title='Click to show phone number in notification'
-                                >{$record->phone}</button>
+                                <span class='text-green-600'>Phone</span>
                             </div>";
                         }
                         
@@ -514,6 +510,15 @@ class BranchAvailabilityIndex extends Page implements HasForms, HasTable
                         return $query;
                     }),
                     
+            ])
+            ->actions([
+                \Filament\Tables\Actions\Action::make('showPhone')
+                    ->label('Show Phone')
+                    ->icon('heroicon-o-phone')
+                    ->color('success')
+                    ->visible(fn (ProviderBranch $record): bool => !empty($record->phone))
+                    ->action(fn (ProviderBranch $record) => $this->showPhoneNotification($record->id))
+                    ->tooltip('Click to show phone number in notification'),
             ])
             ->bulkActions([
                 BulkAction::make('sendAppointmentRequests')
