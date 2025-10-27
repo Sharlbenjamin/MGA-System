8735194b00a57795abdc111f4f48e24f99aec30c
Sharlbenjamin <sharlhany@gmail.com>
2025-10-27 15:42:37 +0300

    Our Network

--- app/Filament/Pages/OurNetwork.php
+++ app/Filament/Pages/OurNetwork.php
@@ -19,6 +19,7 @@ class OurNetwork extends Page implements HasTable
     protected static ?string $navigationGroup = 'PRM';
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-group';
     protected static ?string $title = 'Our Network';
+    protected static string $view = 'filament-panels::page';
     
     use InteractsWithTable;
 
