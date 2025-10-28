9eb3b22ea24441721fb404ac739135f5fca7d9bb
Sharlbenjamin <sharlhany@gmail.com>
2025-10-28 17:09:02 +0300

    Some feature or fix

--- app/Filament/Resources/FileResource/Pages/ViewFile.php
+++ app/Filament/Resources/FileResource/Pages/ViewFile.php
@@ -346,26 +346,28 @@ class ViewFile extends ViewRecord
                             ->columnSpan(1),
                                     ]),
 
-                        // Current Text (Single Column Width)
-                        Card::make()
-                            ->schema([
-                                TextEntry::make('current_text')
-                                    ->label('Current Text')
-                                    ->state(function ($record) {
-                                        return $this->formatCaseInfo($record);
-                                    })
-                                    ->extraAttributes(['class' => 'whitespace-pre-wrap font-mono'])
-                                    ->suffixAction(
-                                        InfolistAction::make('copy_current_text')
-                                            ->icon('heroicon-o-clipboard-document')
-                                            ->color('gray')
-                                            ->action(function ($record) {
-                                                $text = $this->formatCaseInfo($record);
-                                                $this->copyToClipboard($text, 'Current Text');
-                                            })
-                                    ),
-                            ])
-                            ->columnSpan(1),
+                            // Current Text (Below the 3-column grid)
+                            InfolistSection::make()
+                                ->schema([
+                                    Card::make()
+                                        ->schema([
+                                            TextEntry::make('current_text')
+                                                ->label('Current Text')
+                                                ->state(function ($record) {
+                                                    return $this->formatCaseInfo($record);
+                                                })
+                                                ->extraAttributes(['class' => 'whitespace-pre-wrap font-mono'])
+                                                ->suffixAction(
+                                                    InfolistAction::make('copy_current_text')
+                                                        ->icon('heroicon-o-clipboard-document')
+                                                        ->color('gray')
+                                                        ->action(function ($record) {
+                                                            $text = $this->formatCaseInfo($record);
+                                                            $this->copyToClipboard($text, 'Current Text');
+                                                        })
+                                                ),
+                                        ]),
+                                ]),
                             ]),
                         Tab::make('Documents')
                             ->schema($this->getDocumentsTabContent()),
