b870b1a57909f0941c942b8338aa7cf0dce14e79
Sharlbenjamin <sharlhany@gmail.com>
2026-02-22 17:24:39 +0200

    Network

--- app/Filament/Resources/ProviderLeadResource/Widgets/PotentialProviderLeadsByCountryWidget.php
+++ app/Filament/Resources/ProviderLeadResource/Widgets/PotentialProviderLeadsByCountryWidget.php
@@ -3,66 +3,100 @@
 namespace App\Filament\Resources\ProviderLeadResource\Widgets;
 
 use App\Models\ProviderLead;
+use Carbon\Carbon;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Database\Eloquent\Builder;
 
 class PotentialProviderLeadsByCountryWidget extends BaseWidget
 {
+    protected function getColumns(): int
+    {
+        return 4;
+    }
+
     protected function getStats(): array
     {
-        return [
-            Stat::make('UK + Ireland (New)', $this->countNewLeadsThisMonth(['GB', 'IE']))
-                ->description('Created this month')
-                ->color('info'),
-            Stat::make('France (New)', $this->countNewLeadsThisMonth(['FR']))
-                ->description('Created this month')
-                ->color('info'),
-            Stat::make('Italy (New)', $this->countNewLeadsThisMonth(['IT']))
-                ->description('Created this month')
-                ->color('info'),
-            Stat::make('Spain (New)', $this->countNewLeadsThisMonth(['ES']))
-                ->description('Created this month')
-                ->color('info'),
+        $ukIreland = ['GB', 'UK', 'IE', 'United Kingdom', 'Great Britain', 'Ireland'];
+        $france = ['FR', 'France'];
+        $italy = ['IT', 'Italy'];
+        $spain = ['ES', 'Spain'];
 
-            Stat::make('UK + Ireland (Active)', $this->countActiveLeadsThisMonth(['GB', 'IE']))
-                ->description('Last contact this month')
+        return [
+            Stat::make('New - UK & Ireland', $this->countNewLeadsThisMonth($ukIreland))
+                ->description('Created this month')
+                ->descriptionIcon('heroicon-m-sparkles')
+                ->color('primary'),
+            Stat::make('New - France', $this->countNewLeadsThisMonth($france))
+                ->description('Created this month')
+                ->descriptionIcon('heroicon-m-sparkles')
+                ->color('info'),
+            Stat::make('New - Italy', $this->countNewLeadsThisMonth($italy))
+                ->description('Created this month')
+                ->descriptionIcon('heroicon-m-sparkles')
+                ->color('warning'),
+            Stat::make('New - Spain', $this->countNewLeadsThisMonth($spain))
+                ->description('Created this month')
+                ->descriptionIcon('heroicon-m-sparkles')
                 ->color('success'),
-            Stat::make('France (Active)', $this->countActiveLeadsThisMonth(['FR']))
+
+            Stat::make('Active - UK & Ireland', $this->countActiveLeadsThisMonth($ukIreland))
                 ->description('Last contact this month')
+                ->descriptionIcon('heroicon-m-bolt')
+                ->color('primary'),
+            Stat::make('Active - France', $this->countActiveLeadsThisMonth($france))
+                ->description('Last contact this month')
+                ->descriptionIcon('heroicon-m-bolt')
                 ->color('success'),
-            Stat::make('Italy (Active)', $this->countActiveLeadsThisMonth(['IT']))
+            Stat::make('Active - Italy', $this->countActiveLeadsThisMonth($italy))
                 ->description('Last contact this month')
-                ->color('success'),
-            Stat::make('Spain (Active)', $this->countActiveLeadsThisMonth(['ES']))
+                ->descriptionIcon('heroicon-m-bolt')
+                ->color('warning'),
+            Stat::make('Active - Spain', $this->countActiveLeadsThisMonth($spain))
                 ->description('Last contact this month')
+                ->descriptionIcon('heroicon-m-bolt')
                 ->color('success'),
         ];
     }
 
-    protected function baseQuery(array $countryIsoCodes): Builder
+    protected function baseQuery(array $countryKeys): Builder
     {
         return ProviderLead::query()
             ->join('providers', 'providers.id', '=', 'provider_leads.provider_id')
             ->join('countries', 'countries.id', '=', 'providers.country_id')
             ->where('providers.status', 'Potential')
-            ->whereIn('countries.iso', $countryIsoCodes);
+            ->where(function (Builder $query) use ($countryKeys) {
+                $query
+                    ->whereIn('countries.iso', $countryKeys)
+                    ->orWhereIn('countries.name', $countryKeys)
+                    ->orWhereIn('countries.nicename', $countryKeys);
+            });
     }
 
-    protected function countNewLeadsThisMonth(array $countryIsoCodes): int
+    protected function countNewLeadsThisMonth(array $countryKeys): int
     {
-        return (clone $this->baseQuery($countryIsoCodes))
-            ->whereMonth('provider_leads.created_at', now()->month)
-            ->whereYear('provider_leads.created_at', now()->year)
+        [$startOfMonth, $endOfMonth] = $this->currentMonthRange();
+
+        return (clone $this->baseQuery($countryKeys))
+            ->whereBetween('provider_leads.created_at', [$startOfMonth, $endOfMonth])
             ->count('provider_leads.id');
     }
 
-    protected function countActiveLeadsThisMonth(array $countryIsoCodes): int
+    protected function countActiveLeadsThisMonth(array $countryKeys): int
     {
-        return (clone $this->baseQuery($countryIsoCodes))
+        [$startOfMonth, $endOfMonth] = $this->currentMonthRange();
+
+        return (clone $this->baseQuery($countryKeys))
             ->whereNotNull('provider_leads.last_contact_date')
-            ->whereMonth('provider_leads.last_contact_date', now()->month)
-            ->whereYear('provider_leads.last_contact_date', now()->year)
+            ->whereBetween('provider_leads.last_contact_date', [$startOfMonth, $endOfMonth])
             ->count('provider_leads.id');
+    }
+
+    protected function currentMonthRange(): array
+    {
+        return [
+            Carbon::now()->startOfMonth(),
+            Carbon::now()->endOfMonth(),
+        ];
     }
 }
